2df4a21d3af641ad836c3f77f5641b83a64fad95
Andrew <84029016+AndrewSae@users.noreply.github.com>
2022-09-24 11:00:49 -0400

    Update selfDrivingTest.py

--- tests/selfDrivingTest.py
+++ tests/selfDrivingTest.py
@@ -19,89 +19,31 @@ while run:
 
     sleep(0.1)
     if front_sensor.distance * 100 <= 20:
+        #stop the robot
         robot.stop()
-
+        # get the readings from the front left and right sensors
         front_read = front_sensor.distance * 100
-
         left_read = left_sensor.distance * 100
         right_read = right_sensor.distance * 100
 
-        
+        # print out all of the readings 
         print("stoped somthing blocking front")
         print("Front: " + str(front_read))
         print("left: " + str(left_read))
         print("right: " + str(right_read))
 
 
-
         if left_read <= right_read and left_read >= 20:
-            robot.left()
-            sleep(3)
+            while front_sensor.distance * 100 <= 20:
+                robot.left()
             robot.stop()
-            if front_sensor.distance * 100 <=20:
-                  print("stuck")
-
-
+            
         elif right_read >= left_read and right_read >= 20:
-            robot.right()
-            sleep(3)
+            while front_sensor.distance * 100 <= 20:
+                robot.right()
             robot.stop()
-            if front_sensor.distance * 100 <=20:
-                print("stuck")
-
-
-      
-
-    elif front_right_sensor.distance * 100 <= 20:
-        robot.stop()
-
-        front_read = front_sensor.distance * 100
-        front_right_read = front_right_sensor.distance * 100
-        front_left_read = front_left_sensor.distance * 100
-        
-
-        print("stoped somthing blocking right side")
-        print("Right: " + str(front_right_read))
-
-
-        if front_left_read and front_read >= 20:
-            robot.left()
-            sleep(2)
-            robot.stop()
-        else:
-            while front_left_read and front_read <= 20:
-                robot.backward()
-            robot.stop()
-            robot.left()
-            sleep(2)
-            robot.stop()
-
-
-    elif front_left_sensor.distance * 100 <= 20:
-        robot.stop()
-
-        front_read = front_sensor.distance * 100
-        front_right_read = front_right_sensor.distance * 100
-        front_left_read = front_left_sensor.distance * 100
-        
-
-        print("stoped somthing blocking left side")
-        print("Left: " + str(front_left_read))
 
         
-
-        if front_right_read and front_read >= 20:
-            robot.left()
-            sleep(2)
-            robot.stop()
-        else:
-            while front_right_read and front_read <= 20:
-                robot.backward()
-            robot.stop()
-            robot.right()
-            sleep(2)
-            robot.stop()
-
 
     else:
-        robot.forward()
+        robot.forward(speed=.5)
